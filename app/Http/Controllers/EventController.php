e7779a2d7d6c06bc1e5089e41447b68c433f4464
devEliasJr <eliasengmecanica@gmail.com>
2023-10-22 14:10:32 -0300

    feat: Include Event page by Id

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -29,6 +29,7 @@ class EventController extends Controller
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
+        $event->items = $request->items;
 
         //image Upload
         if ($request->hasFile('image') && $request->file('image')->isValid()) {
@@ -46,6 +47,14 @@ class EventController extends Controller
         $event->save();
 
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
+
+    }
+
+    public function show($id) {
+
+        $event = Event::findOrFail($id);
+
+        return view('events.show', ['event' => $event]);
 
     }
 }
