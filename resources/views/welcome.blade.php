68c7579a6ad86e80c5881e7582f580617ae6180d
devEliasJr <eliasengmecanica@gmail.com>
2023-10-22 01:05:22 -0300

    feat: Image upload included

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,12 +16,12 @@
     <div id="cards-container" class="row">
         @foreach($events as $event)
         <div class="card col-md-3">
-            <img src="/img/event_placeholder.jpg" alt="{{ $event->title }}">
-            <div class="card-body">
+            <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
+            <div class="card-body d-flex flex-column">
                 <p class="card-date">10/09/2020</p>
                 <h5 class="card-title">{{ $event->title }}</h5>
                 <p class="card-participants">X Participantes</p>
-                <a href="#" class="btn btn-primary">Saber mais</a>
+                <a href="#" class="btn btn-primary mt-auto">Saber mais</a>
             </div>
         </div>
         @endforeach
