428b9642a13c179c87af6dfab08659909cd37c4f
devEliasJr <eliasengmecanica@gmail.com>
2023-10-22 14:09:33 -0300

    refactor: gitignore and ionicon import

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -56,6 +56,7 @@ name="viewport" content="width=device-width, initial-scale=1">
 
 
     <script src="/js/scripts.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 
